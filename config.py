9437f61bea31d63610e67508f69573ee68ecdb22
holden-holsinger-aca <holden.holsinger@acaglobal.com>
2026-01-20 09:04:49 -0500

    switched to .env file for api key vs windows vars

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 
+
+load_dotenv()
 
 # API Configuration
 BASE_URL = "https://acaalpha.atlassian.net/rest/api/2"
 EMAIL = "holden.holsinger@acaglobal.com"
 
 # must get the API Key from Jira and use correct key to access it below
-API_KEY = os.getenv("jira_token")
+API_KEY = os.getenv("JIRA_TOKEN")
 AUTH = HTTPBasicAuth("holden.holsinger@acaglobal.com", API_KEY)
 
 # Headers
